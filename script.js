cd3154beea8370f2465614bc233cb7485ae3008e
denysMoon <denysulvanskyi@gmail.com>
2021-01-07 00:14:42 +0200

    add button scroll up

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function openMenu() {
-    document.getElementById("sidebar").classList.toggle('active')
+var t;
+function up() {
+    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0,-100);
+        t = setTimeout('up()',20);
+    } else clearTimeout(t);
+    return false;
 }
